9098353f440a35c2bd72fa7ad3c3be30a8cbacfe
Margarita Shishkova <margolen239@gmail.com>
2024-07-23 20:33:55 +0100

    Create Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import './App.module.scss';
-import CarouselCard from './components/CarouselCard/CarouselCard';
 import { Header } from './components/Header';
-import Carousel from './components/carousel/Carousel';
-
-import bouquetImage from './assets/images/bouquets/bouquet-1.png';
+import ReadyBouquet from './components/ReadyBouquet/ReadyBouquet';
 
 import styles from './App.module.scss';
 
 function App() {
-  const bouquet = {
-    price: '10 000',
-    img: bouquetImage,
-  };
-
-  const bouquets = Array.from({ length: 12 }, () => ({ ...bouquet }));
-  const cards = bouquets.map((item) => <CarouselCard {...item} />);
-
   return (
     <>
       <div className={styles.app}>
         <Header />
-        <Carousel items={cards} />
+        <ReadyBouquet />
       </div>
     </>
   );
--- src/components/ReadyBouquet/ReadyBouquet.jsx
+++ src/components/ReadyBouquet/ReadyBouquet.jsx
@@ -1,16 +1,29 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, EffectCoverflow } from 'swiper/modules';
 
+import CarouselCard from '../CarouselCard/CarouselCard';
+import Container from '../Container/Container';
+
+import bouquetImage from '../../assets/images/bouquets/bouquet-1.png';
+
 import 'swiper/scss';
 import 'swiper/scss/pagination';
 import 'swiper/scss/navigation';
 import 'swiper/scss/effect-coverflow';
 
-import styles from './Carousel.module.scss';
+import styles from './ReadyBouquet.module.scss';
 
-function Carousel({ items }) {
+function ReadyBouquet() {
+  const bouquet = {
+    price: '10 000',
+    img: bouquetImage,
+  };
+
+  const bouquets = Array.from({ length: 12 }, () => ({ ...bouquet }));
+  const items = bouquets.map((item) => <CarouselCard {...item} />);
+
   return (
-    <div className={styles.wrapper}>
+    <Container>
       <h3>Готовые букеты</h3>
       <Swiper
         effect={'coverflow'}
@@ -21,8 +34,7 @@ function Carousel({ items }) {
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
-          scale: 0.75,
-          depth: 100,
+          scale: 0.8,
           modifier: 1,
           slideShadows: false,
         }}
@@ -43,8 +55,8 @@ function Carousel({ items }) {
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+    </Container>
   );
 }
 
-export default Carousel;
+export default ReadyBouquet;
