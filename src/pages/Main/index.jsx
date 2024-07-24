8c6a99adfa05fdf350a5a2d8b0d49556def23b24
Nadia <isslandia@mail.ru>
2024-07-24 18:37:01 +0300

    Revert "Revert 8 modal card"

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
-import { ModalCard } from "../../components/ModalCard/ModalCard";
+import React, { useState } from 'react';
+import { ModalCard } from '../../components/ModalCard/ModalCard';
+import MainBanner from '../../components/Main/MainBanner/MainBanner';
+import Categories from '../../components/Main/Categories/Categories';
+import Container from '../../components/Container/Container';
+import ReadyBouquet from '../../components/ReadyBouquet/ReadyBouquet';
+import Title from '../../components/Title/Title';
+
 function Main() {
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
   return (
-    ///кнопка для вызова модального окна товара
     <div>
-      <button onClick={() => setModalIsOpen(true)}>Заказать</button>
-      <ModalCard
-        isOpen={modalIsOpen}
-        onClose={() => setModalIsOpen(false)}
-      ></ModalCard>
+      <MainBanner />
+      <Title />
+      <div>
+        <button onClick={() => setModalIsOpen(true)}>Заказать</button>
+        <ModalCard isOpen={modalIsOpen} onClose={() => setModalIsOpen(false)}></ModalCard>
+      </div>
+      <Container>
+        <ReadyBouquet />
+        <Categories />
+      </Container>
     </div>
   );
 }
