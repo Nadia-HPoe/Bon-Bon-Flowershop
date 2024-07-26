04816a85a35631776af44f8e15b113a92e79c14f
Daria Demidova <daashenkaa@mail.ru>
2024-07-26 23:42:44 +0300

    update header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,35 @@
 import { Icons } from "../Icons";
 import Logo from "../../assets/images/logo.svg";
-import headerStyles from "./header.module.scss";
+import styles from "./header.module.scss";
 
-function Header() {
+const Header = () => {
     return (
         <div>
-            <header className={headerStyles.header}>
-                <div className={headerStyles.header__container}>
-                    <div className={headerStyles.header__leftContainer}>
-                        <div className={headerStyles.header__logo}><img src={Logo} alt="logo" /></div>
+            <header className={styles.header}>
+                <div className={styles.header__container}>
+                    <div className={styles.header__leftContainer}>
+                        <div className={styles.header__logo}><img src={Logo} alt="logo" /></div>
                         <nav>
-                            <ul className={headerStyles.header__menu}>
-                                <li className={headerStyles.header__menuItem}><a href="#" className={headerStyles.header__menuLink}>Каталог</a></li>
-                                <li className={headerStyles.header__menuItem}><a href="#" className={headerStyles.header__menuLink}>О нас</a></li>
-                                <li className={headerStyles.header__menuItem}><a href="#" className={headerStyles.header__menuLink}>Блог</a></li>
+                            <ul className={styles.header__menu}>
+                                <li className={styles.header__menuItem}><a href="#" className={styles.header__menuLink}>Каталог</a></li>
+                                <li className={styles.header__menuItem}><a href="#" className={styles.header__menuLink}>О нас</a></li>
+                                <li className={styles.header__menuItem}><a href="#" className={styles.header__menuLink}>Блог</a></li>
                             </ul>
                         </nav>
                     </div>
-                    <div className={headerStyles.header__rightContainer}>
-                        <div className={headerStyles.header__search}>
+                    <div className={styles.header__rightContainer}>
+                        <div className={styles.header__search}>
                             <Icons name="magnifying-glass" size="26" color="#042423" />
-                            <input type="text" className={headerStyles.header__input}/>
+                            <input type="text" className={styles.header__input}/>
                         </div>
-                        <div className={headerStyles.header__phone}>
-                            <div className={headerStyles.header__phoneNumber}>+7 999 111 22 33</div>
+                        <div className={styles.header__phone}>
+                            <div className={styles.header__phoneNumber}>+7 999 111 22 33</div>
                             <a href="https://wa.me/message/CGNNUIQSEJQYB1"><Icons name="phone" size="35" color="#023E3B" /></a>
                         </div>
-                        <div className={headerStyles.header__icons}>
-                            <a href="#" className={headerStyles.header__icon}><Icons name="user" size="28" color="#727272" /></a>
-                            <a href="#" className={headerStyles.header__icon}><Icons className={headerStyles.icon__heart} name="heart" size="22" color="#EBEBEB" /></a>
-                            <a href="#" className={headerStyles.header__icon}><Icons name="shopping-cart" size="32" color="#727272" /></a>
+                        <div className={styles.header__icons}>
+                            <a href="#" className={styles.header__icon}><Icons name="user" size="28" color="#727272" /></a>
+                            <a href="#" className={styles.header__icon}><Icons className={headerStyles.icon__heart} name="heart" size="22" color="#EBEBEB" /></a>
+                            <a href="#" className={styles.header__icon}><Icons name="shopping-cart" size="32" color="#727272" /></a>
                         </div>
                     </div>    
                 </div>
