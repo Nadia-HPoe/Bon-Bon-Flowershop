5d6e955321b273af1544df75feca1ce0542fc141
Dari-Dari <me@daria-fadeeva.ru>
2024-08-06 04:04:37 +0300

    fix fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import { Header } from './components/Header/Header';
+import { Main } from './pages/Main';
+import { FooterMain } from './components/Footer/FooterMain';
 
-import { Header } from "./components/Header/Header";
-import { Main } from "./pages/Main";
-import { FooterMain } from "./components/Footer/FooterMain";
-
-import "./App.module.scss";
+import './App.module.scss';
 import styles from './App.module.scss';
 
 function App() {
