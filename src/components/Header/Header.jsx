597d57b87dd0b4c53bcef230cd290c79c274c0df
Daria Demidova <daashenkaa@mail.ru>
2024-08-11 13:23:15 +0300

    fix icons in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -40,25 +40,22 @@ const Header = () => {
           </div>
           <div className={styles.header__rightContainer}>
             <div className={styles.header__search}>
-              <Icons name="magnifying-glass" size="26" color="#042423" />
+              <Icons name="magnifying-glass" size="24" color="#042423" />
               <input type="text" className={styles.header__input} />
             </div>
             <div className={styles.header__phone}>
               <div className={styles.header__phoneNumber}>+7 999 111 22 33</div>
               <a href="https://wa.me/message/CGNNUIQSEJQYB1">
-                <Icons name="phone" size="35" color="#023E3B" />
+                <Icons name="phone" size="34" color="#023E3B" />
               </a>
             </div>
             <div className={styles.header__icons}>
-              <a href="#" className={styles.header__icon}>
+              <button href="#" className={styles.header__icon}>
                 <Icons name="user" size="28" color="#727272" />
-              </a>
+              </button>
               <a href="#" className={styles.header__icon}>
-                <Icons className={styles.icon__heart} name="heart" size="22" color="#EBEBEB" />
+                <Icons className={styles.header__heart} name="heart" size="22" color="#EBEBEB" />
               </a>
-              {/* <a href="#" className={styles.header__icon}>
-                <Icons name="shopping-cart" size="32" color="#727272" />
-              </a> */}
               <button className={styles.header__icon} onClick={() => setCartIsOpen(true)}>
                 <Icons name="shopping-cart" size="32" color="#727272" />
               </button>
