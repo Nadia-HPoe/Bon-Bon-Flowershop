3a526563387b99e7ad6cebc6a39146ac1aaf9b88
Nadia-HPoe <isslandia@mail.ru>
2024-10-14 19:54:49 +0300

    fix cart design

--- src/components/ModalCart/Cart.jsx
+++ src/components/ModalCart/Cart.jsx
@@ -23,32 +23,26 @@ const Cart = ({ isOpen, onClose }) => {
           <Icons name="shopping-cart" size="300" color="#F2F2F2" />
         </div>
         <div className="cart-container cart-full">
-          <h1 className="cart__title">Ваша корзина</h1>
-
+          <h1 className="cart__title">В КОРЗИНЕ:</h1>
+          <button className="button__close" onClick={() => onClose()}>
+            <Icons className="icon__close" name="cross" size="28" color="gray" />
+          </button>
           <div className="cart-list__product">
             <div className="cart-list__product_card">
               <img className="cart-list__product_image" src={Image} alt="" />
               <div className="cart-list__product_info">
-                <h2>Название букета</h2>
-                <p>Размер S (маленький)</p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet urna at
-                  purus ullamcorper, non iaculis diam sagittis. Nulla mi orci, feugiat eu vulputate
-                  id, dignissim sed ex. Integer cursus luctus odio ut condimentum. Integer
-                  ullamcorper sed ipsum id commodo.
-                </p>
+                <h2 className="cart-list__product_title">Название букета</h2>
                 <div className="cart-list__product_bottom">
-                  <div className="cart-list__buttons">
-                    <button className="cart-list__button_less">-</button>
-                    <input className="cart-list__amount_input" type="text" value="1" />
-                    <button className="cart-list__button_more">+</button>
+                  <div className="cart-list__bottom-info">
+                    <p className="cart-list__product_amount">Количество: 1шт</p>
+                    <p className="cart-list__product_price">1 000₽</p>
                   </div>
-                  <p className="cart-list__product_price">1 000₽</p>
-                  <button className="cart-list__product_delete">
+
+                  <button className="cart-list__product_like">
                     <Icons
-                      className="cart-list__product_delete-icon"
-                      name="trash"
-                      size="20"
+                      className="cart-list__product_like-icon"
+                      name="like"
+                      size="55"
                       color="gray"
                     />
                   </button>
@@ -56,13 +50,10 @@ const Cart = ({ isOpen, onClose }) => {
               </div>
             </div>
             <div className="cart-list_order">
-              <button className="cart-list_order_button">Оформить заказ на 1 000 ₽</button>
+              <button className="cart-list_order_button">ЗАКАЗАТЬ</button>
             </div>
           </div>
         </div>
-        <button className="button__close" onClick={() => onClose()}>
-          <Icons className="icon__close" name="cross" size="28" color="gray" />
-        </button>
       </div>
     </Modal>
   );
