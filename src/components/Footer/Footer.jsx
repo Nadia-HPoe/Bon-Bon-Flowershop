dc8ad550fcea48935c4319931bf78b36aa684a88
Bema <138109349+Bemadler@users.noreply.github.com>
2024-10-28 00:18:35 +0600

    update all

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,99 +1,21 @@
-// import styles from './Footer.module.scss';
-// import { Icons } from '../Icons/Icons';
-// import logo from '../../assets/logo/Bon_bon_logo.png';
-// import flower from '../../assets/picture/flower.svg';
-
-// function Footer() {
-//   return (
-//     <footer className={styles.footer}>
-//       <div className={styles.footer__container}>
-//         <div className={styles.footer__contacts}>
-//           <div className={styles.logos__wrapper}>
-//             <img src={logo} alt="logo" className={styles.logo} />
-//             <p className={styles.footer__title}>flowers boutique</p>
-//           </div>
-//         </div>
-//         <div className={styles.footer__contacts}>
-//           <div className={styles.contacts__wrapper}>
-//             <a
-//               className={styles.contacts__phone}
-//               href="tel:+7 903 849 2777"
-//               aria-label="Позвонить по телефону +7 903 888 9977"
-//             >
-//               <p>+7 903 849 2777</p>
-//             </a>
-//             <a className={styles.contacts__mail} href="bon-bonnn@yandex.ru">
-//               <p>bon-bonnn@yandex.ru</p>
-//             </a>
-//             <div className={styles.contacts__address}>
-//               <p>г. Нижний Новгород,</p>
-//               <p>пл. Свободы, д.7</p>
-//             </div>
-//           </div>
-//         </div>
-//         <div className={styles.footer__contacts}>
-//           <div className={styles.contacts__working__hours}>
-//             <p className={styles.contacts__working__hours__title}>График работы:</p>
-//             <p className={styles.contacts__working__hours__text}>
-//               <p>Пн-Пт с 9:00 до 20:00</p>
-//               <p> Сб-Вс с 10:00 до 18:00</p>
-//             </p>
-//           </div>
-//           <div className={styles.contacts__icons}>
-//             <a
-//               className={styles.contacts__whatsapp__icon}
-//               href="https://api.whatsapp.com/send/?phone=%2B79038492777&text&type=phone_number&app_absent=0"
-//             >
-//               <Icons name="whatsapp" size="40" color="#ffebce" />
-//             </a>
-//             <a className={styles.contacts__vk__icon} href="https://vk.com/bon_bon52">
-//               <Icons name="vk" size="40" color="#ffebce" />
-//             </a>
-//           </div>
-//         </div>
-//         <div className={styles.footer__flower}>
-//           <img src={flower} alt="flower" className={styles.flower} />
-//         </div>
-//       </div>
-//       <hr className={styles.footer__divider} />
-//       <div className={styles.footer__conrainer__team}>
-//         <div className={styles.policy__wrapper}>
-//           <a className={styles.policy__offer} href="">
-//             <p>Оферта</p>
-//           </a>
-//           <a className={styles.policy__policy} href="">
-//             <p>Политика</p>
-//           </a>
-//           <a className={styles.policy__delivery__payment} href="">
-//             <p>Доставка и оплата</p>
-//           </a>
-//         </div>
-//         <a className={styles.contacts__team} href="">
-//           <p>Ⓒ Команда</p>
-//         </a>
-//       </div>
-//     </footer>
-//   );
-// }
-
-// export { Footer };
-import styles from './FooterMain.module.scss';
+import styles from './Footer.module.scss';
 import { Icons } from '../Icons/Icons';
-
-import flower from '../../assets/picture/flower.svg';
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.footer_text_block}>
-        <p className={styles.footer_text}>
+      <div className={styles.footer__text__block}>
+          <p className={styles.footer__text}>
           Мы работаем с лучшими поставщиками в городе, и тщательно отбираем лучший цветок для наших
           любимых клиентов.
-        </p>
-        <p className={styles.footer_text}>
-          Наша команда ответственно подходит к сборке каждого букета. Мы готовы в любой момент
-          порадовать Ваших близкий замечательными букетами или композициями.
-        </p>
+          </p>
+          <p className={styles.footer__text}>
+          Наша команда ответственно подходит к сборке каждого букета.
+          </p>
+          <p className={styles.footer__text}>
+          Мы готовы в любой момент порадовать Ваших близкий замечательными букетами или композициями.
+          </p>
       </div>
       <div className={styles.footer__container}>
         <div className={styles.footer__map__wrapper}>
@@ -133,8 +55,8 @@ function Footer() {
             </a>
             <iframe
               src="https://yandex.ru/map-widget/v1/?from=api-maps&ll=44.004658%2C56.305212&mode=search&oid=217049081924&ol=biz&origin=jsapi21&sll=44.016551%2C56.319245&sspn=0.068064%2C0.023133&text=%D0%A6%D0%B2%D0%B5%D1%82%D0%BE%D1%87%D0%BD%D0%B0%D1%8F%20%D0%BC%D0%B0%D1%81%D1%82%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F%20Bon-Bon&z=13"
-              width="560"
-              height="400"
+              width="600"
+              height="474"
               frameBorder="1"
               allowFullScreen="true"
               style={{ position: 'relative' }}
@@ -186,24 +108,24 @@ function Footer() {
             </div>
           </div>
           <div className={styles.policy__wrapper}>
-            <a className={styles.policy__offer} href="#">
+            <Link to="/catalogue" className={styles.policy__offer}>
               <p>Каталог</p>
-            </a>
-            <a className={styles.policy__offer} href="#">
+            </Link>
+            <Link to="/about" className={styles.policy__offer} >
               <p>О нас</p>
-            </a>
-            <a className={styles.policy__offer} href="#">
+            </Link>
+            <Link to="/blog" className={styles.policy__offer} >
               <p>Блог</p>
-            </a>
-            <a className={styles.policy__offer} href="#">
+            </Link>
+            <Link to="" className={styles.policy__offer} >
               <p>Оферта</p>
-            </a>
-            <a className={styles.policy__policy} href="#">
+            </Link>
+            <Link to="" className={styles.policy__policy} >
               <p>Политика</p>
-            </a>
-            <a className={styles.policy__delivery__payment} href="#">
+            </Link>
+            <Link to="/delivery" className={styles.policy__delivery__payment} >
               <p>Доставка и оплата</p>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
