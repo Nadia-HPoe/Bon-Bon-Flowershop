95bf9ca3043a18c306bff5eabbce5fa299ad5769
Nadia-HPoe <isslandia@mail.ru>
2024-10-30 19:25:44 +0300

    fix review widget

--- src/components/Main/Reviews/Reviews.jsx
+++ src/components/Main/Reviews/Reviews.jsx
@@ -2,14 +2,15 @@ import styles from './Reviews.module.scss';
 
 const Reviews = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.reviews}>
       <h2 className={styles.title}>Отзывы</h2>
-      <div>
+      <div className={styles.container}>
         <iframe
           className={styles.frame}
           src="https://swdgts.ru/8e53d3d9a72a544f034226288d6f1ca8"
-          style={{ position: 'relative' }}
         ></iframe>
+        {/* <script src="https://res.smartwidgets.ru/app.js" defer></script>
+        <div className="sw-app" data-app="8e53d3d9a72a544f034226288d6f1ca8"></div> */}
       </div>
     </div>
   );
