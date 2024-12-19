8e5b65bd7e6efceaa6678c51a59a6dd557c0b4b1
Bema <138109349+Bemadler@users.noreply.github.com>
2024-12-19 18:27:13 +0600

    Category and Banner
    Fixed Category section
    Fixed Main banner section

--- src/components/Main/MainBanner/MainBanner.jsx
+++ src/components/Main/MainBanner/MainBanner.jsx
@@ -1,10 +1,14 @@
 import styles from "./MainBanner.module.scss";
+import logo from "./../../../assets/logo/Bon_bon_logo.png";
+
 
 const MainBanner = () => {
   return (
     <div className={styles.mainBanner_container}>
-      <div className={styles.mainBanner_buttonContainer}>
-        <button className={styles.mainBanner_button}>Заказать</button>
+      <div className={styles.mainBanner_container_logo}>
+        <img className={styles.mainBanner_logo} src={logo} alt="logo" />
+        <h1 className={styles.mainBanner_text}>Собираем букеты с любовью</h1>
+          <button className={styles.mainBanner_button}>Заказать</button>
       </div>
     </div>
   );
